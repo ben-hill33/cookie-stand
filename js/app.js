9458ca6dd54d1449b2c740ec096dfbdc5a21236d
BEN <benjamin.hill28@gmail.com>
2020-04-09 11:58:57 -0700

    added function to evaluate sum of cookiesPerHour

--- js/app.js
+++ js/app.js
@@ -5,21 +5,17 @@ console.log('im alive');
 // -Number of cookies made depends on hours of operations which is 6-8 for all locations, plust a few factors unique to each location such as:
   // Minimum number of customers per hour
   // Maximum number of customers per hour
-  // Average number of cookies solder per customer
+  // Average number of cookies sold per customer
 // -Needs to be adaptable
 // -User needs to be able to easily modify the input numbers for each location based on day of the week, special events, and other factors. 
 // -User would like to see nice formatting for the numbers
 
-//Instructions for sales data page
-var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','Total'];
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
 var seattle = {
   minCustHours: 23,
   maxCustHours: 65,
   avgCookies: 6.3,
   storeCookiePerHour: [],
-  hourlyTotal: function(){
-    return (this.storeCookiePerHour + storeHours);
-  },
   randomNumberCustomerHour: function(){
     return (Math.floor(Math.random() * (this.maxCustHours - this.minCustHours +1)) + this.minCustHours);//returns a random number between 23 and 65
   },
@@ -27,14 +23,22 @@ var seattle = {
     for(var i = 0; i < storeHours.length; i++){
       this.storeCookiePerHour.push(this.randomNumberCustomerHour() * this.avgCookies);
     }
+  },
+  cookieSum: function(){
+    var arraySum = 0;
+    for(var i = 0; i < storeHours.length; i++){
+      arraySum = arraySum + this.storeCookiePerHour[i];
+    }
+    return arraySum;
   }
-
 
 };
 console.log(seattle);
 seattle.cookiePerHour();
-seattle.hourlyTotal();
+seattle.cookieSum();
 
-//TODO: Calculating the sum of these hourly total
+
+
+//TODO: Use template literals to display Seattle store's hours and cookies per hour on sales.html
 
 
